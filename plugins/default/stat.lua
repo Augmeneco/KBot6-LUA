dacc91eb983073f1fb877e8479f396a1be080062
Cha14ka <taitor@yandex.ru>
2019-08-03 04:54:55 +0300

    Доп инфа в статистике

--- plugins/default/stat.lua
+++ plugins/default/stat.lua
@@ -10,16 +10,18 @@ return function(msg)
         &#8195;Использовано: RAM_USE МБ
         &#8195;Использовано ботом: BOT_USE кБ
         Бот:
+        &#8195;Обработано сообщений: MSGS
+        &#8195;Обработано команд: CMDS
         &#8195;Время работы: WORK_TIME
     ]]
     status = status:gsub('BOT_USE',BOT_USE):gsub('UPTIME',io.popen('uptime'):read('*a'))
-    local WORK_TIME = math.floor((os.time()-msg.bot_stat.work_start)/(60*60*24))..' дней | '..math.floor((os.time()-msg.bot_stat.work_start)/(60*60))..' часов | '
-    WORK_TIME = WORK_TIME..math.floor((os.time()-msg.bot_stat.work_start)/(60))..' минут | '..math.floor(os.time()-msg.bot_stat.work_start)..' секунд'
+    local WORK_TIME = math.floor((os.time()-msg.bot_stat.work_start)/(60*60*24))..' дней | '..(math.floor((os.time()-msg.bot_stat.work_start)/(60*60))%60)..' часов | '
+    WORK_TIME = WORK_TIME..(math.floor((os.time()-msg.bot_stat.work_start)/(60))%60)..' минут | '..(math.floor((os.time()-msg.bot_stat.work_start))%60)..' секунд'
 
     RAM = {}
     for i in io.popen('free -m'):read('*a'):gmatch('%d+') do RAM[#RAM+1]=i if #RAM==2 then break end end
     status = status:gsub('RAM_FULL',RAM[1]):gsub('RAM_USE',RAM[2])
-
+    status = status:gsub('MSGS',msg.bot_stat.msgs):gsub('CMDS',msg.bot_stat.cmds)
     status = status:gsub('WORK_TIME',WORK_TIME):gsub('TEMP',io.open('/sys/class/thermal/thermal_zone0/temp','r'):read('*a')/1000)
     libkb.apisay(status,msg.toho)
 end
